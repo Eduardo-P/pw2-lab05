833e0c024a08069bdf11d7a183e54b3315f15fbd
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-20 15:45:00 -0500

    Metodo join() terminado, para colocar una figura al lado de la figura actual

--- picture.py
+++ picture.py
@@ -36,7 +36,10 @@ class Picture:
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    return Picture(None)
+    join = []
+    for i in range(len(self.img)):
+      join.append(self.img[i] + p.img[i]) 
+    return Picture(join)
 
   def up(self, p):
     return Picture(None)
