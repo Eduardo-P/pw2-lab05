c852614e45f1e4806717e6943fd36f5ab2f3644a
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-21 19:52:00 -0500

    Agregando el metodo horizontalRepeat() terminado, para repetir una imagen n veces horizontalmente

--- picture.py
+++ picture.py
@@ -63,7 +63,10 @@ class Picture:
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    horizontalRepeat = []
+    for value in self.img:
+      horizontalRepeat.append(value*n)
+    return Picture(horizontalRepeat)
 
   def verticalRepeat(self, n):
     return Picture(None)
