489b38a926cb5b31da1c5d5073015f4954a5baec
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-22 21:31:00 -0500

    Completando el ejercicio g, teniendo ya las filas de peones y piezas mayores, creo el resto del tablero de ajedrez

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -8,4 +8,8 @@ rowPawns = row.under(pawn.horizontalRepeat(8))
 rowPieces = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
 rowPieces = row.negative().under(rowPieces)
 
-draw(rowPieces.up(rowPawns))
+table = rowPieces.up(rowPawns)
+table = table.up(row.negative().up(row).verticalRepeat(2))
+table = table.up(rowPawns.up(rowPieces).negative())
+
+draw(table)
