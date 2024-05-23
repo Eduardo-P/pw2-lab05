3261473ac9714228564042c80a25264ef2b5a0e5
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-22 20:09:00 -0500

    Completando el ejercicio a, creadno los 4 caballos

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+horseWhite = knight
+horseBlack = horseWhite.negative()
+row = horseBlack.join(horseWhite)
+table = row.up(row.negative())
+draw(table)
