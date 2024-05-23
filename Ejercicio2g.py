47cf6ccb2e2fd2f4b0a3065b583b627c55c9168f
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-22 21:23:00 -0500

    Completando el ejercicio g, creando fila de peones y fila de piezas mayores

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+row = square.join(square.negative()).horizontalRepeat(4)
+
+rowPawns = row.under(pawn.horizontalRepeat(8))
+
+rowPieces = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+rowPieces = row.negative().under(rowPieces)
+
+draw(rowPieces.up(rowPawns))
