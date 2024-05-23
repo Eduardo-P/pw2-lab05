c3adf9ed714cb254d3d8032b6240f196b64d1a47
Eduardo <eportugalpor@unsa.edu.pe>
2024-05-22 20:25:00 -0500

    Completando el ejercicio b, creadno los 4 caballos, junto con color invertido y reflejo vertical

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+horseWhite = knight
+horseBlack = horseWhite.negative().verticalMirror()
+row = horseBlack.up(horseWhite)
+table = row.join(row.negative())
+draw(table)
